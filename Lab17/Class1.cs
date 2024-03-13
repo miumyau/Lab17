395712fd1bc39b9442d0f7ed10d9f492d73beaca
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-13 09:45:25 +0300

    Добавьте файлы проекта.

---
+++ Lab17/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class  BankAccount<T>
+    {
+        private int balance;
+        private string name;
+        private T number;
+        public int Balance { get { return balance; } set { balance = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public T Number { get { return number; } set { number = value; } }
+
+        public void Input() 
+        {
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите номер счета");
+            Number = (T) Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance= Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void GetInfo() 
+        {
+            Console.WriteLine($"Фио:{Name}, Номер счета:{Number}, Баланс счета:{Balance}");
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<string> account1 = new BankAccount<string>();
+            BankAccount<int> account2 = new BankAccount<int>();
+            account1.Input();
+            account2.Input();
+            account1.GetInfo();
+            account2.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
